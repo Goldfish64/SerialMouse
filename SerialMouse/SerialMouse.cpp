1ac1e6c37e04b04f3977fce532676b32dceb6c76
John Davis <goldfishnet64@outlook.com>
2023-05-08 21:44:29 -0500

    Cleanup formatting and switch to MacKernelSDK

--- SerialMouse/SerialMouse.cpp
+++ SerialMouse/SerialMouse.cpp
@@ -1,240 +1,306 @@
-/*
- * File: SerialMouse.cpp
- *
- * Copyright (c) 2018-2019 John Davis
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+//
+//  SerialMouse.cpp
+//  Serial mouse driver for macOS.
+//
+//  Copyright © 2018-2023 Goldfish64. All rights reserved.
+//
 
 #include "SerialMouse.hpp"
 
-// This required macro defines the class's constructors, destructors,
-// and several other methods I/O Kit requires.
 OSDefineMetaClassAndStructors(SerialMouseResources, IOService)
 OSDefineMetaClassAndStructors(SerialMouse, IOHIPointing)
 
 IOService *SerialMouse::probe(IOService *provider, SInt32 *score) {
-    DBGLOG("SerialMouse::probe(): start\n");
-    if (!super::probe(provider, score))
-        return NULL;
-    
-    // Create variables.
-    IORS232SerialStreamSync *serialStream = (IORS232SerialStreamSync*)provider;
-    UInt32 origDataRate = 0, origDataSize = 0, origStopBits = 0, origFlowControl = 0;
-    IOService *returnService = NULL;
-    
-    // Acquire port.
-    if (acquirePort(serialStream) != kIOReturnSuccess)
-        goto fail;
-    
-    // Save original port settings, setup stream, and check mouse ID
-    if ((getPortSettings(serialStream, &origDataRate, &origDataSize, &origStopBits, &origFlowControl) != kIOReturnSuccess) ||
-        (setupPort(serialStream) != kIOReturnSuccess) || (checkMouseId(serialStream) != kIOReturnSuccess))
-        goto fail;
-    
-    // There is a serial mouse present, so we can match.
-    returnService = this;
-    goto done;
-    
-fail:
-    DBGLOG("SerialMouse::probe(): no mouse found\n");
-    returnService = NULL;
-    
-done:
-    // Close port and restore port settings.
-    setPortSettings(serialStream, origDataRate, origDataSize, origStopBits, origFlowControl);
-    releasePort(serialStream);
-    return returnService;
+  DBGLOG("SerialMouse: probe()\n");
+  if (!super::probe(provider, score)) {
+    return nullptr;
+  }
+
+  IOReturn status;
+  bool probed = false;
+  IORS232SerialStreamSync *serialStream = OSDynamicCast(IORS232SerialStreamSync, provider);
+  UInt32 origDataRate = 0, origDataSize = 0, origStopBits = 0, origFlowControl = 0;
+  
+  if (serialStream == nullptr) {
+    SYSLOG("SerialMouse: Provider is not a serial stream\n");
+    return nullptr;
+  }
+
+  //
+  // Acquire serial port.
+  //
+  if (acquirePort(serialStream) != kIOReturnSuccess) {
+    return nullptr;
+  }
+
+  //
+  // Save original port settings, setup stream, and check mouse ID.
+  //
+  do {
+    status = getPortSettings(&origDataRate, &origDataSize, &origStopBits, &origFlowControl);
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Failed to set serial port settings during probe()\n");
+      break;
+    }
+
+    status = setupPort();
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Failed to setup serial port during probe()\n");
+      break;
+    }
+
+    status = checkMouseId();
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Device on serial port is not a serial mouse\n");
+      break;
+    }
+
+    probed = true;
+  } while (false);
+
+  //
+  // Restore port settings and release serial port.
+  //
+  setPortSettings(origDataRate, origDataSize, origStopBits, origFlowControl);
+  releasePort();
+  return probed ? this : nullptr;
 }
 
 bool SerialMouse::start(IOService *provider) {
-    DBGLOG("SerialMouse::start(): start\n");
-    if (!super::start(provider))
-        return false;
-    
-    // Acquire port, setup stream, and check mouse ID.
-    serialStream = (IORS232SerialStreamSync*)provider;
-    if ((acquirePort(serialStream) != kIOReturnSuccess) || (setupPort(serialStream) != kIOReturnSuccess))
-        goto fail;
-    
-    // Initialize polling thread.
-    if (kernel_thread_start(OSMemberFunctionCast(thread_continue_t, this, &SerialMouse::pollMouseThread),
-                            this, &pollThread) != kIOReturnSuccess)
-        goto fail;
-    
-    // Driver started successfully.
-    return true;
-    
-fail:
-    DBGLOG("SerialMouse::start(): failed to start mouse\n");
-    releasePort(serialStream);
+  DBGLOG("SerialMouse: Starting\n");
+
+  IOReturn status;
+  bool started = false;
+  IORS232SerialStreamSync *serialStream = OSDynamicCast(IORS232SerialStreamSync, provider);
+
+  if (serialStream == nullptr) {
+    SYSLOG("SerialMouse: Provider is not a serial stream\n");
     return false;
+  }
+
+  if (!super::start(provider)) {
+    return false;
+  }
+
+  //
+  // Setup port and start polling.
+  //
+  do {
+    status = acquirePort(serialStream);
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Failed to acquire serial port\n");
+      break;
+    }
+
+    status = setupPort();
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Failed to setup serial port during probe()\n");
+      break;
+    }
+
+    status = checkMouseId();
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Device on serial port is not a serial mouse\n");
+      break;
+    }
+
+    status = kernel_thread_start(OSMemberFunctionCast(thread_continue_t, this, &SerialMouse::pollMouseThread),
+                                 this, &_pollThread);
+    if (status != kIOReturnSuccess) {
+      SYSLOG("SerialMouse: Polling thread could not be created\n");
+      break;
+    }
+
+    started = true;
+    SYSLOG("SerialMouse: Serial mouse started\n");
+  } while (false);
+
+  if (!started) {
+    stop(provider);
+  }
+  return started;
 }
 
 void SerialMouse::stop(IOService *provider) {
-    DBGLOG("SerialMouse::stop(): start\n");
-    
-    // Kill polling thread.
-    thread_terminate(pollThread);
-    thread_deallocate(pollThread);
-    
-    // Stop superclass.
-    super::stop(provider);
+  //
+  // Kill polling thread.
+  //
+  thread_terminate(_pollThread);
+  thread_deallocate(_pollThread);
+  releasePort();
+
+  super::stop(provider);
 }
 
 void SerialMouse::pollMouseThread(void) {
-    DBGLOG("SerialMouse::pollMouseThread(): start\n");
-    while (true) {
-        UInt8 packet[MOUSE_PACKET_LENGTH];
-        UInt32 count = 0;
-        
-        // Read packet.
-        if ((serialStream->dequeueData(packet, MOUSE_PACKET_LENGTH, &count, MOUSE_PACKET_LENGTH) == kIOReturnSuccess) &&
-            count == MOUSE_PACKET_LENGTH) {
-            DBGLOG("SerialMouse::pollMouseThread(): got packet %X %X %X\n",
-                   packet[0], packet[1], packet[2]);
-            
-            // If first byte is invalid, flush buffer.
-            if (!MOUSE_PACKET_VALID(packet))
-                flushPort(serialStream);
-            else { // Packet is valid, send to HID system.
-                // Get current time.
-                uint64_t now_abs;
-                clock_get_uptime(&now_abs);
-                uint64_t now_ns;
-                absolutetime_to_nanoseconds(now_abs, &now_ns);
+  DBGLOG("SerialMouse: Polling thread\n");
+  while (true) {
+    UInt8 packet[MOUSE_PACKET_LENGTH];
+    UInt32 count = 0;
 
-                // Dispatch pointer movement event.
-                dispatchRelativePointerEvent(MOUSE_PACKET_POSX(packet), MOUSE_PACKET_POSY(packet),
-                                             MOUSE_PACKET_BUTTONS(packet), *(AbsoluteTime*)&now_ns);
-            }
-        }
+    //
+    // Read incoming packet.
+    //
+    if ((_serialStream->dequeueData(packet, MOUSE_PACKET_LENGTH, &count, MOUSE_PACKET_LENGTH) == kIOReturnSuccess)
+      && count == MOUSE_PACKET_LENGTH) {
+      DBGLOG("SerialMouse::pollMouseThread(): got packet %X %X %X\n", packet[0], packet[1], packet[2]);
+
+      // If first byte is invalid, flush buffer.
+      if (!MOUSE_PACKET_VALID(packet)) {
+        flushPort();
+      } else { // Packet is valid, send to HID system.
+        // Get current time.
+        uint64_t now_abs;
+        clock_get_uptime(&now_abs);
+        uint64_t now_ns;
+        absolutetime_to_nanoseconds(now_abs, &now_ns);
+
+        //
+        // Dispatch pointer movement event.
+        //
+        dispatchRelativePointerEvent(MOUSE_PACKET_POSX(packet), MOUSE_PACKET_POSY(packet),
+                                     MOUSE_PACKET_BUTTONS(packet), *(AbsoluteTime*)&now_ns);
+      }
     }
+  }
 }
 
 IOReturn SerialMouse::acquirePort(IORS232SerialStreamSync *serialStream) {
-    DBGLOG("SerialMouse::acquirePort(): start\n");
-    
-    // Acquire port.
-    return serialStream->acquirePort(false);
+  DBGLOG("SerialMouse: Acquiring serial port\n");
+
+  IOReturn status = serialStream->acquirePort(false);
+  if (status != kIOReturnSuccess) {
+    SYSLOG("SerialMouse: Failed to acquire serial port with status 0x%X\n", status);
+    return status;
+  }
+
+  _serialStream = serialStream;
+  _serialStream->retain();
+  return status;
 }
 
-IOReturn SerialMouse::releasePort(IORS232SerialStreamSync *serialStream) {
-    DBGLOG("SerialMouse::releasePort(): start\n");
-    IOReturn status;
-    
-    // Deactivate port.
-    status = serialStream->executeEvent(PD_E_ACTIVE, false);
-    if (status != kIOReturnSuccess)
-        return status;
-    
-    // Release port.
-    return serialStream->releasePort();
+void SerialMouse::releasePort() {
+  DBGLOG("SerialMouse: Releasing serial port\n");
+
+  //
+  // Deactivate and release port.
+  //
+  if (_serialStream != nullptr) {
+    if (_serialStream->executeEvent(PD_E_ACTIVE, false) == kIOReturnSuccess) {
+      _serialStream->releasePort();
+    }
+  }
+  OSSafeReleaseNULL(_serialStream);
 }
 
-IOReturn SerialMouse::setupPort(IORS232SerialStreamSync *serialStream) {
-    DBGLOG("SerialMouse::setupPort(): start\n");
-    IOReturn status;
-    
-    // Set up port.
-    status = setPortSettings(serialStream, MOUSE_DATA_RATE, MOUSE_DATA_SIZE, MOUSE_STOP_BITS, MOUSE_FLOW_CONTROL);
-    if (status != kIOReturnSuccess)
-        return status;
-    
-    // Activate port.
-    return serialStream->executeEvent(PD_E_ACTIVE, true);
+IOReturn SerialMouse::setupPort() {
+  DBGLOG("SerialMouse: Setting up port\n");
+  IOReturn status;
+
+  //
+  // Set up and activate port.
+  //
+  status = setPortSettings(MOUSE_DATA_RATE, MOUSE_DATA_SIZE, MOUSE_STOP_BITS, MOUSE_FLOW_CONTROL);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  return _serialStream->executeEvent(PD_E_ACTIVE, true);
 }
 
-IOReturn SerialMouse::flushPort(IORS232SerialStreamSync *serialStream) {
-    DBGLOG("SerialMouse::flushPort(): start\n");
-    return serialStream->executeEvent(PD_E_RXQ_FLUSH, 0);
+IOReturn SerialMouse::flushPort() {
+  DBGLOG("SerialMouse: Flushing port\n");
+  return _serialStream->executeEvent(PD_E_RXQ_FLUSH, 0);
 }
 
-IOReturn SerialMouse::checkMouseId(IORS232SerialStreamSync *serialStream) {
-    DBGLOG("SerialMouse::checkMouseId(): start\n");
-    IOReturn status;
-    UInt8 mouseId = 0;
-    UInt32 count = 0;
-    
-    // Flush receive buffer.
-    status = flushPort(serialStream);
-    if (status != kIOReturnSuccess)
-        return status;
-    
-    // Toggle DTR bit.
-    status = serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL & ~PD_RS232_S_DTR);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL);
-    if (status != kIOReturnSuccess)
-        return status;
-    
-    // Read ID byte.
-    IOSleep(MOUSE_ID_DELAY_MS);
-    status = serialStream->dequeueData(&mouseId, 1, &count, 0);
-    DBGLOG("SerialMouse::checkMouseId(): device returned ID byte 0x%X\n", mouseId);
-    if (status != kIOReturnSuccess)
-        return status;
-    
-    // Ensure mouse ID byte is valid.
-    if (mouseId != MOUSE_ID_BYTE)
-        return kIOReturnInvalid;
-    
-    // Success.
-    return kIOReturnSuccess;
+IOReturn SerialMouse::checkMouseId() {
+  DBGLOG("SerialMouse: Checking mouse ID\n");
+  IOReturn status;
+  UInt8 mouseId = 0;
+  UInt32 count = 0;
+
+  //
+  // Flush receive buffer.
+  //
+  status = flushPort();
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+
+  //
+  // Toggle DTR bit.
+  //
+  status = _serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL & ~PD_RS232_S_DTR);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->executeEvent(PD_E_FLOW_CONTROL, MOUSE_FLOW_CONTROL);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+
+  //
+  // Read ID byte.
+  //
+  IOSleep(MOUSE_ID_DELAY_MS);
+  status = _serialStream->dequeueData(&mouseId, 1, &count, 0);
+  DBGLOG("SerialMouse::checkMouseId(): device returned ID byte 0x%X\n", mouseId);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+
+  //
+  // Ensure mouse ID byte is valid.
+  //
+  if (mouseId != MOUSE_ID_BYTE) {
+    return kIOReturnInvalid;
+  }
+  return kIOReturnSuccess;
 }
 
-IOReturn SerialMouse::getPortSettings(IORS232SerialStreamSync *serialStream, UInt32 *dataRate,
-                         UInt32 *dataSize, UInt32 *stopBits, UInt32 *flowControl) {
-    DBGLOG("SerialMouse::getPortSettings(): start\n");
-    IOReturn status;
-    
-    // Get port settings.
-    status = serialStream->requestEvent(PD_E_DATA_RATE, dataRate);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->requestEvent(PD_E_DATA_SIZE, dataSize);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->requestEvent(PD_RS232_E_STOP_BITS, stopBits);
-    if (status != kIOReturnSuccess)
-        return status;
-    return serialStream->requestEvent(PD_E_FLOW_CONTROL, flowControl);
+IOReturn SerialMouse::getPortSettings(UInt32 *dataRate, UInt32 *dataSize, UInt32 *stopBits, UInt32 *flowControl) {
+  DBGLOG("SerialMouse: Get port settings\n");
+  IOReturn status;
+
+  //
+  // Get port settings.
+  //
+  status = _serialStream->requestEvent(PD_E_DATA_RATE, dataRate);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->requestEvent(PD_E_DATA_SIZE, dataSize);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->requestEvent(PD_RS232_E_STOP_BITS, stopBits);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  return _serialStream->requestEvent(PD_E_FLOW_CONTROL, flowControl);
 }
 
-IOReturn SerialMouse::setPortSettings(IORS232SerialStreamSync *serialStream, UInt32 dataRate,
-                         UInt32 dataSize, UInt32 stopBits, UInt32 flowControl) {
-    DBGLOG("SerialMouse::setPortSettings(): start\n");
-    IOReturn status;
-    
-    // Get port settings.
-    status = serialStream->executeEvent(PD_E_DATA_RATE, dataRate);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->executeEvent(PD_E_DATA_SIZE, dataSize);
-    if (status != kIOReturnSuccess)
-        return status;
-    status = serialStream->executeEvent(PD_RS232_E_STOP_BITS, stopBits);
-    if (status != kIOReturnSuccess)
-        return status;
-    return serialStream->executeEvent(PD_E_FLOW_CONTROL, flowControl);
+IOReturn SerialMouse::setPortSettings(UInt32 dataRate, UInt32 dataSize, UInt32 stopBits, UInt32 flowControl) {
+  DBGLOG("SerialMouse: Set port settings(%u,%u,%u,%u)\n", dataRate, dataSize, stopBits, flowControl);
+  IOReturn status;
+
+  //
+  // Set port settings.
+  //
+  status = _serialStream->executeEvent(PD_E_DATA_RATE, dataRate);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->executeEvent(PD_E_DATA_SIZE, dataSize);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  status = _serialStream->executeEvent(PD_RS232_E_STOP_BITS, stopBits);
+  if (status != kIOReturnSuccess) {
+    return status;
+  }
+  return _serialStream->executeEvent(PD_E_FLOW_CONTROL, flowControl);
 }
--- SerialMouse/SerialMouse.hpp
+++ SerialMouse/SerialMouse.hpp
@@ -1,38 +1,19 @@
-/*
- * File: SerialMouse.hpp
- *
- * Copyright (c) 2018-2019 John Davis
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+//
+//  SerialMouse.hpp
+//  Serial mouse driver for macOS.
+//
+//  Copyright © 2018-2023 Goldfish64. All rights reserved.
+//
 
 #ifndef SerialMouse_hpp
 #define SerialMouse_hpp
 
-// Common I/O Kit includes.
 #include <IOKit/IOLib.h>
 #include <IOKit/IOService.h>
 #include <IOKit/IOTypes.h>
 #include <IOKit/IOCommandGate.h>
 #include <IOKit/IOTimerEventSource.h>
 
-// HID and serial includes.
 #include <IOKit/hidsystem/IOHIPointing.h>
 #include <IOKit/serial/IORS232SerialStreamSync.h>
 
@@ -43,10 +24,13 @@
 #define DBGLOG(args...) ;
 #endif
 
-// Bits.
+#define SYSLOG(args...) IOLog(args)
+
 #define bits <<1
 
+//
 // Serial settings for MS protocol.
+//
 #define MOUSE_DATA_RATE     (1200 bits)
 #define MOUSE_DATA_SIZE     (7 bits)
 #define MOUSE_STOP_BITS     (1 bits)
@@ -60,6 +44,7 @@
 #define HID_MOUSE_LEFTB     0x1
 #define HID_MOUSE_RIGHTB    0x2
 
+//
 // Serial mouse packet format:
 //
 // 7  6  5  4  3  2  1  0
@@ -68,7 +53,9 @@
 // X  0  Y5 Y4 Y3 Y2 Y1 Y0
 //
 
+//
 // Serial mouse packet stuff.
+//
 #define MOUSE_PACKET_LENGTH     3
 #define MOUSE_PACKET_HEADER_BIT 0x40
 #define MOUSE_PACKET_LEFTB_BIT  0x20
@@ -78,43 +65,49 @@
 #define MOUSE_PACKET_LEFTB(packet)      ((Boolean)(packet[0] & MOUSE_PACKET_LEFTB_BIT))
 #define MOUSE_PACKET_RIGHTB(packet)     ((Boolean)(packet[0] & MOUSE_PACKET_RIGHTB_BIT))
 #define MOUSE_PACKET_BUTTONS(packet)    ((UInt32)((MOUSE_PACKET_LEFTB(packet) ? HID_MOUSE_LEFTB : 0) | \
-    (MOUSE_PACKET_RIGHTB(packet) ? HID_MOUSE_RIGHTB : 0)))
+  (MOUSE_PACKET_RIGHTB(packet) ? HID_MOUSE_RIGHTB : 0)))
 #define MOUSE_PACKET_POSX(packet)       ((SInt8)((packet[1] & 0x3F) | ((packet[0] & 0x3) << 6)))
 #define MOUSE_PACKET_POSY(packet)       ((SInt8)((packet[2] & 0x3F) | ((packet[0] & 0xC) << 4)))
 
+//
 // SerialMouseResources class. This is used to keep the kext in memory.
+//
 class SerialMouseResources : IOService {
-    OSDeclareDefaultStructors(SerialMouseResources);
+  OSDeclareDefaultStructors(SerialMouseResources);
 };
 
-// SerialMouse class.
 class SerialMouse : IOHIPointing {
-    typedef IOHIPointing super;
-    OSDeclareDefaultStructors(SerialMouse);
-    
-public:
-    virtual IOService *probe(IOService *provider, SInt32 *score);
-    virtual bool start(IOService *provider);
-    virtual void stop(IOService *provider);
-    
+  typedef IOHIPointing super;
+  OSDeclareDefaultStructors(SerialMouse);
+
 private:
-    // Serial stream.
-    IORS232SerialStreamSync *serialStream;
-    
-    // Polling thread.
-    thread_t pollThread;
-    void pollMouseThread(void);
-    
-    static IOReturn acquirePort(IORS232SerialStreamSync *serialStream);
-    static IOReturn releasePort(IORS232SerialStreamSync *serialStream);
-    static IOReturn setupPort(IORS232SerialStreamSync *serialStream);
-    static IOReturn flushPort(IORS232SerialStreamSync *serialStream);
-    static IOReturn checkMouseId(IORS232SerialStreamSync *serialStream);
-    
-    static IOReturn getPortSettings(IORS232SerialStreamSync *serialStream, UInt32 *dataRate,
-                                    UInt32 *dataSize, UInt32 *stopBits, UInt32 *flowControl);
-    static IOReturn setPortSettings(IORS232SerialStreamSync *serialStream, UInt32 dataRate,
-                                    UInt32 dataSize, UInt32 stopBits, UInt32 flowControl);
+  //
+  // Serial stream.
+  //
+  IORS232SerialStreamSync *_serialStream = nullptr;
+
+  //
+  // Polling thread.
+  //
+  thread_t _pollThread = nullptr;
+  void pollMouseThread();
+
+  IOReturn acquirePort(IORS232SerialStreamSync *serialStream);
+  void releasePort();
+  IOReturn setupPort();
+  IOReturn flushPort();
+  IOReturn checkMouseId();
+
+  IOReturn getPortSettings(UInt32 *dataRate, UInt32 *dataSize, UInt32 *stopBits, UInt32 *flowControl);
+  IOReturn setPortSettings(UInt32 dataRate, UInt32 dataSize, UInt32 stopBits, UInt32 flowControl);
+
+public:
+  //
+  // IOService overrides.
+  //
+  virtual IOService *probe(IOService *provider, SInt32 *score) APPLE_KEXT_OVERRIDE;
+  virtual bool start(IOService *provider) APPLE_KEXT_OVERRIDE;
+  virtual void stop(IOService *provider) APPLE_KEXT_OVERRIDE;
 };
 
 #endif
